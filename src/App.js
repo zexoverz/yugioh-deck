d14ee469bfb4a8772ed1fa1ba48e8b13a282380d
Faisal <aryuical@gmail.com>
2019-11-25 17:58:57 +0700

    Styling

--- src/App.js
+++ src/App.js
@@ -5,35 +5,46 @@ import React, {
 import './App.css';
 import axios from "axios";
 import CardYugi from "./components/cardYugi";
+import Swal from "sweetalert2";
+import logo2 from "./logo2.png";
 
 
 
 const App = () =>  {
   
-  const [title,setTitle] = useState("Yu Gi Oh World")
+  // const [title,setTitle] = useState("Yu Gi Oh World")
   const [yugioh, setYugi] = useState([]);
   const [search, setSearch] = useState("");
   
   
   useEffect(async () => {
+
+    Swal.fire({
+      title: 'Loading...'
+    });
+    Swal.showLoading();
     const response = await axios({
       method: "GET",
       url: "https://db.ygoprodeck.com/api/v5/cardinfo.php"
     })
     let cards = response.data
-
     setYugi(cards.slice(500,552))
+    Swal.close();
   }, [])
 
 
   const handleChange = (event) => {
   setSearch(event.target.value)
-
   }
   
   
  const fetchYugi = async (e, searchYugi) => {
    e.preventDefault();
+
+    Swal.fire({
+      title: 'Loading...'
+    });
+    Swal.showLoading();
     const response = await axios({
       method: "GET",
       url: "https://db.ygoprodeck.com/api/v5/cardinfo.php"
@@ -43,19 +54,20 @@ const App = () =>  {
     return card.name.toLowerCase().includes(searchYugi.toLowerCase())
     })
     setYugi(filtered)
+    Swal.close();
  }
   
   return (
-    <div className="container text-center">
-        <h2 className="mr-5 mt-5">{title}</h2>
-
-        <h4 className="mr-5 mt-5">Find Some Cards</h4>
+    <div className="container text-center text-white">
+        {/* <h2 className="mr-5 mt-5">{title}</h2> */}
+        <img src={logo2} width="400px;" className="mr-3"></img>
+        <h4 className="mr-5 ">Find Some Cards</h4>
         <form onSubmit={(e) => fetchYugi(e, search)}>
           <input value={search} onChange={handleChange}  type="text" className="mr-2 col-4"></input>
-          <button className="btn btn-success" type="submit">Find Cards</button>
+          <button className="btn btn-dark" type="submit">Find Cards</button>
         </form>
         
-        <CardYugi yugioh={yugioh}></CardYugi>
+        <CardYugi yugioh={yugioh} ></CardYugi>
         
         
       </div>
--- src/components/cardYugi.js
+++ src/components/cardYugi.js
@@ -9,7 +9,7 @@ const cardYugi = (props) => {
         <div id="cardList" className="row mt-5 ">
         {
             props.yugioh.map(card => 
-            <Card style={{ width: '18rem' }} className="col-3 " key={card.id}>
+            <Card style={{ width: '18rem' }} className="col-3 bg-dark" key={card.id}>
             <Card.Img variant="top" src={card.card_images[0].image_url} />
             <Card.Body>
             <Button variant="primary">Add to Deck</Button>
