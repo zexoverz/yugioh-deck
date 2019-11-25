bf2e6b5bc04100c6ae1a1862906b1fe15d5bb2ed
Faisal <aryuical@gmail.com>
2019-11-25 16:20:47 +0700

    add Search Feature and implement React Hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {
-  Component
+  Component,useState,useEffect
 } from 'react';
 // import logo from './logo.svg';
 import './App.css';
@@ -7,66 +7,61 @@ import { Card } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 import axios from "axios";
 
-const fetchYugi = async () => {
-  const response = await axios({
-    method: "GET",
-    url: "https://db.ygoprodeck.com/api/v5/cardinfo.php"
-  })
-
-  return response.data
-}
 
 
 
+const App = () =>  {
 
-class App extends Component { 
+ 
+  
+  const [title,setTitle] = useState("Yu Gi Oh World")
+  const [yugioh, setYugi] = useState([]);
+  const [search, setSearch] = useState("");
+  
+  
 
-  constructor(){
-    super()
-
-    this.state = {
-      title: "Yu Gi Oh World",
-      yugioh: [],
-      search: ""
-    };
+  const handleChange = (event) => {
+    setSearch(event.target.value)
   }
 
-  // filteredList() {
-  //   return this.postList.filter(post => {
-  //     return post.title.toLowerCase().includes(this.search.toLowerCase())
-  //   })
-  // }
 
-  async componentDidMount() {
-    let cards = await fetchYugi()
+  // useEffect(async () => {
+   
+  // }, ["search"])
+  
+  
+ const fetchYugi = async (e, searchYugi) => {
+   e.preventDefault();
+    const response = await axios({
+      method: "GET",
+      url: "https://db.ygoprodeck.com/api/v5/cardinfo.php"
+    })
+    let cards = response.data
     let filtered = cards.filter(card => {
-      return card.name.toLowerCase().includes(this.state.search.toLowerCase())
+    return card.name.toLowerCase().includes(searchYugi.toLowerCase())
     })
-    this.setState({
-      yugioh: filtered
-    })
-
-    
-  }
-
-  handleChange(event){
-    this.setState({
-      search: event.target.value
-    })
-  }
-
-  render() {
-    return (
-      <div className="container text-center">
+    // if (search.length > 1){
+    //   setYugi(filtered)
+    // }
+    console.log("trigerred")
+    setYugi(filtered)
+ }
+  
+  return (
+    <div className="container text-center">
         {/* <h1 className="mr-5">faisal</h1> */}
-        <h2 className="mr-5 mt-5">{this.state.title}</h2>
+        <h2 className="mr-5 mt-5">{title}</h2>
 
         <h4 className="mr-5 mt-5">Find Some Cards</h4>
-        <input value={this.state.search} onChange={this.handleChange.bind(this)} type="text" className="mr-5 col-4"></input>
+        <form onSubmit={(e) => fetchYugi(e, search)}>
+          <input value={search} onChange={handleChange}  type="text" className="mr-2 col-4"></input>
+          <button className="btn btn-success" type="submit">Find Cards</button>
+        </form>
+        
         
         <div id="cardList" className="row mt-5 ">
         {
-          this.state.yugioh.map(card => 
+          yugioh.map(card => 
             <Card style={{ width: '18rem' }} className="col-3 " key={card.id}>
             <Card.Img variant="top" src={card.card_images[0].image_url} />
             <Card.Body>
@@ -80,24 +75,50 @@ class App extends Component {
           )
         }
 
-
-        {/* <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-        Some quick example text to build on the card title and make up the bulk of
-        the card's content.
-        </Card.Text>
-         </Card.Body>
-        </Card> */}
-
         </div>
         
       </div>
-    );
-  }
+  )
 }
+
+
+
+
+
+// class App extends Component { 
+
+//   constructor(){
+//     super()
+
+//     this.state = {
+//       title: "Yu Gi Oh World",
+//       yugioh: [],
+//       search: ""
+//     };
+//   }
+
+//   // filteredList() {
+//   //   return this.postList.filter(post => {
+//   //     return post.title.toLowerCase().includes(this.search.toLowerCase())
+//   //   })
+//   // }
+
+//   async componentDidMount() {
+    
+//     this.setState({
+//       yugioh: filtered
+//     })
+
+    
+//   }
+
+
+//   render() {
+//     return (
+      
+//     );
+//   }
+// }
 
 export default App;
 
