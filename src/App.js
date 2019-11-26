b5df26fc07a918125e2e328eeb65e9b073502b34
Faisal <aryuical@gmail.com>
2019-11-26 11:10:09 +0700

    add 1 class component

--- src/App.js
+++ src/App.js
@@ -9,28 +9,20 @@ import {
   Route,
   Link
 } from 'react-router-dom';
-import history from "./history";
+
 
 import './App.css';
 
-
-const App = () =>  {
-  
-  
-
-  useEffect(() => {
-    history.push("/")
-  }, [])
-  
-  
-  return (
-    <Router history={history}>
-    <Route exact path="/" component={Home}></Route>
-    <Route exact path="/MyDeck" component={MyDeck}></Route>
+export default class App extends Component {
+  render() {
+    return (
+    <Router >
+      <Route exact path="/" component={Home}></Route>
+      <Route exact path="/MyDeck" component={MyDeck}></Route>
 
     </Router>
-  )
+    )
+  }
 }
 
-export default App;
 
