1d58e7139d3b611d1bb761a2a4d49c1bc7791ec0
Faisal <aryuical@gmail.com>
2019-11-27 17:30:47 +0700

    Finish

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -4,11 +4,19 @@ import { Card } from 'react-bootstrap';
 
 import { Button } from 'react-bootstrap';
 
+import "./cardYugi.css"
+
+import {useHistory} from "react-router-dom";
 
 const Deck = (props) => {
 
+    const history = useHistory();
     const handleDelete = (card) => {
         props.handleDelete(card);
+    }
+
+    const handleDetail = (card) => {
+        history.push(`/Detail/${card.name}`)
     }
 
     return (
@@ -16,7 +24,7 @@ const Deck = (props) => {
         {
             props.deckCards.map(card => 
             <Card style={{ width: '18rem' }} className="col-3 bg-dark" key={card.id}>
-            <Card.Img variant="top" src={card.card_images[0].image_url} />
+            <Card.Img variant="top" id="img" src={card.card_images[0].image_url} onClick={() => handleDetail(card)} />
             <Card.Body>
             <Button variant="danger" onClick={() => handleDelete(card)} >Delete</Button>
             </Card.Body>
